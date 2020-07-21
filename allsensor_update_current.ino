3e16130842b9040bb92a44c3c70df225e4f22965
Md Rubayatur Bhuyian <55252705+rubayat-uw@users.noreply.github.com>
2020-07-21 17:52:27 -0400

    Add files via upload

--- allsensor_update_current.ino
+++ allsensor_update_current.ino
@@ -48,8 +48,8 @@ float Celsius_1 = 0;
 float Celsius_2 = 0;
 
 
-const int AirValue = 920;   //you need to replace this value with Value_1
-const int WaterValue = 393;  //you need to replace this value with Value_2
+const int AirValue = 916;   //you need to replace this value with Value_1
+const int WaterValue = 380;  //you need to replace this value with Value_2
 int soilMoistureValue = 0;
 int soilmoisturepercent=0;
 float m;
@@ -106,15 +106,24 @@ void loop() {
     Serial.println("\nConnected.");
   }
 
-soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
+int soilMoistureValue_2 = 0;
+float k;
+for (int i=0;i<10;i++)
+{  
+soilMoistureValue_2 = analogRead(A0);  //put Sensor insert into soil
+k=soilMoistureValue_2+k;
+delay(100);
+}
+soilMoistureValue=k/100;
 Serial.println(soilMoistureValue);
+
 soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-if(soilmoisturepercent > 100)
+if(soilmoisturepercent >= 100)
 {
   Serial.println("100 %");
   m=100;
 }
-else if(soilmoisturepercent <0)
+else if(soilmoisturepercent <=0)
 {
   Serial.println("0 %");
   m=0;
